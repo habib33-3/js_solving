3cf7c7dbe1173c95b5c3f011205cc4b1478f9337
Habibur Rahman <habib.swi16@gmail.com>
2023-08-06 21:35:44 +0600

    centimeterToMeter

---
+++ centimeterToMeter.js
@@ -0,0 +1,11 @@
+function centimeterToMeter(cm) {
+  let m = cm / 100;
+
+  return m;
+}
+
+let cm = 800;
+
+let m = centimeterToMeter(cm);
+
+console.log(m);
--- feetToInch.js
+++ feetToInch.js
@@ -1,11 +1,11 @@
 function feetToInch(feet) {
-  inch = feet / 12;
+  let inch = feet / 12;
 
   return inch;
 }
 
 let feet = 60;
 
-inch = feetToInch(feet);
+let inch = feetToInch(feet);
 
 console.log(inch);
